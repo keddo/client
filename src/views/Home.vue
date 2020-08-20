77a40e37ef64f8fb1f1031c300672e95d00c9430
keddo <adu.kedir.abdurahman@gmail.com>
2020-08-20 17:57:16 +0300

    Add post message to the database

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,26 +1,27 @@
 <template>
   <div class="home">
-    <div class="container-fluid">
-      <form>
-  <div class="form-group">
-    <label for="username">User Name:</label>
-    <input type="text" class="form-control" id="username" placeholder="Anonymous" required>
-  </div>
-  <div class="form-group">
-    <label for="subject">Subject:</label>
-    <input type="text" class="form-control" id="subject" placeholder="Subject" required>
-  </div>
-  <div class="form-group">
-    <label for="imageUrl">ImageUrl:</label>
-    <input type="file" class="form-control" id="imageUrl" required>
-  </div>
-  <div class="form-group">
-    <label for="message">Message</label>
-    <textarea class="form-control" id="message" rows="3"></textarea>
-  </div>
-  <button type="submit" class="btn btn-primary">Add Message</button>
+  
+      <div class="row m-5 px-4">
+        <div class="container">
+    <form class="mb-4" @submit.prevent="addMessage">
+    <div class="form-group">
+      <label for="username">User Name:</label>
+      <input v-model="message.username" type="text" class="form-control" id="username" placeholder="Anonymous" required>
+    </div>
+    <div class="form-group">
+      <label for="subject">Subject:</label>
+      <input v-model="message.subject" type="text" class="form-control" id="subject" placeholder="Subject" required>
+    </div>
+    <div class="form-group">
+      <label for="message">Message</label>
+      <textarea v-model="message.message" class="form-control" id="message" rows="3"></textarea>
+    </div>
+    <div class="form-group">
+      <label for="imageUrl">ImageUrl:</label>
+      <input v-model="message.imageUrl" type="url" class="form-control" id="imageUrl" placeholder="Enter you image url" required>
+    </div>
+    <button type="submit" class="btn btn-primary">Add Message</button>
 </form>
-      <div class="row m-5">
         <div class="media mb-3" v-for="message in messages" :key="message._id">
         <img :src="message.imageUrl" class="mr-3" :alt="message.subject" width="200" height="200">
         <div class="media-body">
@@ -41,6 +42,12 @@ export default {
   name: 'Home',
   data: () => ({
     messages: [],
+    message: {
+      username: '',
+      subject: '',
+      message: '',
+      imageUrl: ''
+    }
   }),
   mounted() {
     fetch(API_URL)
@@ -49,5 +56,20 @@ export default {
         this.messages = result;
       });
   },
+  methods: {
+    addMessage() {
+       fetch(API_URL, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(this.message),
+      })
+      .then(response => response.json())
+      .then(data => {
+        console.log('Success:', data);
+      })
+    }
+  }
 };
 </script>
