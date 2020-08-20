102daca2724a7d5166d8b0142ef054d712d0f601
keddo <adu.kedir.abdurahman@gmail.com>
2020-08-20 17:04:35 +0300

    Add message post form

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,25 @@
 <template>
   <div class="home">
     <div class="container-fluid">
+      <form>
+  <div class="form-group">
+    <label for="username">User Name:</label>
+    <input type="text" class="form-control" id="username" placeholder="Anonymous" required>
+  </div>
+  <div class="form-group">
+    <label for="subject">Subject:</label>
+    <input type="text" class="form-control" id="subject" placeholder="Subject" required>
+  </div>
+  <div class="form-group">
+    <label for="imageUrl">ImageUrl:</label>
+    <input type="file" class="form-control" id="imageUrl" required>
+  </div>
+  <div class="form-group">
+    <label for="message">Message</label>
+    <textarea class="form-control" id="message" rows="3"></textarea>
+  </div>
+  <button type="submit" class="btn btn-primary">Add Message</button>
+</form>
       <div class="row m-5">
         <div class="media mb-3" v-for="message in messages" :key="message._id">
         <img :src="message.imageUrl" class="mr-3" :alt="message.subject" width="200" height="200">
